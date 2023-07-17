e6a21d274ac65c0a1b2b55e9f346e155d7b78cbb
Cagdas Can <cagdascan@gmail.com>
2023-07-17 19:25:11 +0300

    day 2, second iteration

--- 2022/rust/day-02/src/lib.rs
+++ 2022/rust/day-02/src/lib.rs
@@ -1,66 +1,50 @@
 pub fn process_part1(input: &str) -> String {
-    let mut choice_score: u32 = 0;
-    let mut game_score: u32 = 0;
+    input
+        .lines()
+        .map(|game| {
+            let player_1_move = game.chars().nth(0).unwrap();
+            let player_2_move = game.chars().nth(2).unwrap();
+            let moves = (player_1_move, player_2_move);
 
-    input.split("\n").for_each(|game| {
-        println!("{:?} vs {:?}", game.chars().nth(0), game.chars().nth(2));
-        let player_1 = game.chars().nth(0).unwrap();
-        let player_2 = game.chars().nth(2).unwrap();
-
-        match player_2 {
-            'X' => {
-                choice_score += 1;
+            match moves {
+                ('A', 'X') => 4,
+                ('A', 'Y') => 8,
+                ('A', 'Z') => 3,
+                ('B', 'X') => 1,
+                ('B', 'Y') => 5,
+                ('B', 'Z') => 9,
+                ('C', 'X') => 7,
+                ('C', 'Y') => 2,
+                ('C', 'Z') => 6,
+                _ => 0,
             }
-            'Y' => {
-                choice_score += 2;
-            }
-            'Z' => {
-                choice_score += 3;
-            }
-            _ => {}
-        }
-
-        match (player_1, player_2) {
-            ('A', 'X') => game_score += 3,
-            ('A', 'Y') => game_score += 6,
-            ('A', 'Z') => game_score += 0,
-            ('B', 'X') => game_score += 0,
-            ('B', 'Y') => game_score += 3,
-            ('B', 'Z') => game_score += 6,
-            ('C', 'X') => game_score += 6,
-            ('C', 'Y') => game_score += 0,
-            ('C', 'Z') => game_score += 3,
-            _ => {}
-        }
-    });
-
-    return (choice_score + game_score).to_string();
+        })
+        .sum::<u32>()
+        .to_string()
 }
 
 pub fn process_part2(input: &str) -> String {
-    let mut game_score: u32 = 0;
+    input
+        .lines()
+        .map(|game| {
+            let player_1_move = game.chars().nth(0).unwrap();
+            let player_2_move = game.chars().nth(2).unwrap();
 
-    input.lines().for_each(|game| {
-        let player_1 = game.chars().nth(0).unwrap();
-        let player_2 = game.chars().nth(2).unwrap();
-
-        match (player_1, player_2) {
-            ('A', 'X') => game_score += 3,
-            ('A', 'Y') => game_score += 4,
-            ('A', 'Z') => game_score += 8,
-
-            ('B', 'X') => game_score += 1,
-            ('B', 'Y') => game_score += 5,
-            ('B', 'Z') => game_score += 9,
-
-            ('C', 'X') => game_score += 2,
-            ('C', 'Y') => game_score += 6,
-            ('C', 'Z') => game_score += 7,
-            _ => {}
-        }
-    });
-
-    return game_score.to_string();
+            match (player_1_move, player_2_move) {
+                ('A', 'X') => 3,
+                ('A', 'Y') => 4,
+                ('A', 'Z') => 8,
+                ('B', 'X') => 1,
+                ('B', 'Y') => 5,
+                ('B', 'Z') => 9,
+                ('C', 'X') => 2,
+                ('C', 'Y') => 6,
+                ('C', 'Z') => 7,
+                _ => 0,
+            }
+        })
+        .sum::<u32>()
+        .to_string()
 }
 
 #[cfg(test)]
